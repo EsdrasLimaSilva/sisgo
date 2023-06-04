7627c46617508e5d0751d02690e2258de18dc2d1
Esdras Silva <esdraslimasilva83@gmail.com>
2023-06-04 09:12:29 -0300

    Add scss variables and mongdb credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.scss";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Sisgo ',
-  description: 'Discuções aprofundadas sobre tecnologia',
-}
+    title: "Sisgo ",
+    description: "Discuções aprofundadas sobre tecnologia",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="pt-BR">
+            <body className={poppins.className}>{children}</body>
+        </html>
+    );
 }
