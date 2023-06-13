bf2c1542bec4149051a3bd2fe05970cef37d4c62
Esdras Silva <esdraslimasilva83@gmail.com>
2023-06-13 09:21:06 -0300

    Style the home page and add dynamic home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HomePost from "@/components/HomePost";
 import { getMongoPosts } from "@/lib/mongoClient";
 import styles from "@/app/styles/home.module.scss";
+import { headers } from "next/dist/client/components/headers";
 
 export default async function Home() {
+    const headersList = headers();
     const posts = await getMongoPosts();
 
     return (
@@ -10,7 +12,7 @@ export default async function Home() {
             <header className={styles.header}>
                 <h1>Sisgo</h1>
             </header>
-            <main>
+            <main className={styles.container}>
                 {posts.map((post) => (
                     <HomePost key={post._id} post={post} />
                 ))}
